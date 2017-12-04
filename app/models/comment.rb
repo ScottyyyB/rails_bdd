481425ac8e5a65512fe63ceeefe9da77fca1d058
ScottB <17sb@cis.dk>
2017-12-04 10:17:43 +0100

    Modified Email REGex validator

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :article
   validates :body, presence: true
-  validates_format_of :email, with: /\A\w[a-zA-Z0-9_.-]+\@[a-z]+\.[a-z]+\Z/i, if: :email?
+  validates_format_of :email, with: /\A\w[a-zA-Z0-9_.-]*\@[a-z]+\.[a-z]+\Z/i, if: :email?
 end
