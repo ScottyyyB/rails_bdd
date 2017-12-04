079ddc400dd23f600f36974ccef48c973da1a44f
ScottB <17sb@cis.dk>
2017-12-04 13:47:30 +0100

    Refactor controller action create for Comment Controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,14 +6,16 @@ class CommentsController < ApplicationController
 
   def create
     @article = Article.find(params[:article_id])
-    @comment = Comment.new(article: @article,
-      body: params[:comment][:body],
-      email: params[:comment][:email])
+    @comment = @article.comments.new(comment_params)
     @comment.save ? comment_created : (render :new)
   end
 
   private
   
+  def comment_params
+    params[:comment].permit(:email, :body)
+  end
+
   def comment_created
     flash[:notice] = "Comment has been successfully created."
     redirect_to root_path
